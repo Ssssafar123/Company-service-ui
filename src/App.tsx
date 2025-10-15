8d33609d31883f39220819884a4c947f1d2d5b5f
CodeWithRohitRajput <example@gmail.com>
2025-10-15 19:23:06 +0530

    setup whole radix ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Routes , Route } from 'react-router-dom'
+import Test from './pages/test.js'
+import Test2 from './pages/Test2.js'
 
 const App = () => {
   return (
     <div>
-      Hey, Welcome to safar
+      <Routes>
+        <Route path='/' element={<Test/>} />
+        <Route path='/test2' element={<Test2/>} />
+      </Routes>
     </div>
   )
 }
