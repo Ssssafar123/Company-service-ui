8f7604a9fb01b99a465f2f00ed1eeeff8fdb7648
CodeWithRohitRajput <rohitsinghrajput2k24@gmail.com>
2025-11-02 00:01:02 +0530

    changes theme color

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -21,7 +21,7 @@ export default function ThemeProvider({ children }: { children: React.ReactNode 
   return (
     <Theme
       appearance={isDark ? "dark" : "light"}
-      accentColor="pink"
+      accentColor="mint"
       grayColor="slate"
       radius="large"
       scaling="100%"
